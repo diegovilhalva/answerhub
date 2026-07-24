afdbf8ab892833a39857195a25a67f701f795ee4
Diego Vilhalva <diegorocha450@yahoo.com>
2026-07-24 09:51:18 -0300

    fix: fix server.js and vercel.json in backend

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ import 'dotenv/config';
 import app from './src/app.js';
 import { connectDB } from './src/config/db.js';
 
-const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
+connectDB().catch(console.error);
+
+
+if (!process.env.VERCEL) {
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`AnswerHub API running on port ${PORT}`);
   });
-});
+}
+
 
 export default app;
